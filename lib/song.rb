2a334af235d71aa211e367ef0e37de997f8f0f73
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-21 18:10:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,11 @@ class Song
 
 attr_accessor :name, :genre, :artist
 
-@@all = []
-
 def initialize(name, genre, artist)
   @name = name
   @genre = genre
   @artist = artist
-  @@all << self
+  @all = {}
 end
 
 
