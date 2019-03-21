dadaad39d91287b6a4e1690d6d652d638acf6a1c
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-21 18:49:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,6 @@ class Song
   @@genres=[]
   @@artists=[]
   @@count=0
-  @@genre_count = {}
 
   attr_accessor :name, :artist, :genre
 
@@ -29,9 +28,15 @@ class Song
   end
 
   def self.genre_count
-    @@genres.each_with_index do |genre,index|
-      binding.pry
-    end
-  end
+     genre_count = {}
+     @@genres.each do |genre|
+       if genre_count[genre]
+         genre_count[genre] += 1
+       else
+         genre_count[genre] = 1
+       end
+     end
+     genre_count
+   end
 
 end
